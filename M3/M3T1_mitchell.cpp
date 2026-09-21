6b7972ab758829676fb283b82c0c7faab0f65150
mitchela2507 <mitchela2507@student.faytechcc.edu>
2026-09-21 14:59:50 +0000

    M3T1 finished

---
+++ M3/M3T1_mitchell.cpp
@@ -0,0 +1,53 @@
+// CSC 134
+// M3T1 - Comparing Areas
+// mitchella
+// 9/21/2026
+// Get area of two rectangles, find the largest one.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Declare variables for each rectangle
+    double length1, width1, area1;
+    double length2, width2, area2;
+
+    // Input: Ask for length and width of both
+    cout << "First rectangle:" << endl;
+    cout << "\tlength ? ";
+    cin  >> length1;
+    cout << "\twidth  ? ";
+    cin  >> width1;  
+
+    cout << "2nd rectangle:" << endl;
+    cout << "\tlength ? ";
+    cin  >> length2;
+    cout << "\twidth  ? ";
+    cin  >> width2;  
+    
+    // another way to do it:
+    //cout << "Enter rectangle 1's dimensions, put a space between them." << endl;
+    //cin  >> length1 >> width1;
+
+    // Calculation: find area of both
+    area1 = length1 * width1;
+    area2 = length2 * width2;
+
+    //Output: print the areas
+    cout << "Rectangle 1 is area: " << area1 << endl;
+    cout << "Rectangle 2 is area: " << area2 << endl;
+
+    // TODO: Tell the user which one is larger (more area)
+    // is statements test for true or false
+    if (area1 > area2){
+        cout << "Rectangle 1 is larger." << endl;
+    }
+    else if (area2 > area1){
+        cout << "Rectangle 2 is larger." << endl;
+    }
+    else {
+        cout << "They are the same size." << endl;
+    }
+
+    return 0; // no errors
+}
